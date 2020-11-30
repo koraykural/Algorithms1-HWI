ee9e668adf18a158eee03200ed94b56d93336f3d
koraykural <koraykural99@gmail.com>
2020-11-30 10:50:02 +0300

    Read, sort, print duration

--- main.cpp
+++ main.cpp
@@ -1,33 +1,39 @@
-#include<iostream>
-#include<fstream>
-#include<string>
+#include <time.h>
+#include <chrono>
+#include "./include/OrderList.h"
 
 using namespace std;
 
-int main(void){
-	
-	ifstream file;
-	file.open("sales.txt");
-	
-	if (!file){
-		cerr << "File cannot be opened!";
-		exit(1);
-		}
-		
-	int N = 100; //you should read value of N from command line
-	string line;
-	
-	getline(file, line); //this is the header line
-
-	for(int i = 0; i<N; i++){
-		
-		getline(file, line, '\t'); //country (string)
-		getline(file, line, '\t'); //item type (string)
-		getline(file, line, '\t'); //order id (string)
-		file >> line; //units sold (integer)
-		file >> line; //total profit (float)
-	    getline(file, line, '\n'); //this is for reading the \n character into dummy variable.
+int main(int argc, char *argv[])
+{
+	// Check command line arguments
+	if (argc != 2)
+	{
+		cerr << "N argument is not provided." << endl;
+		return 1;
 	}
-	
+
+	// Parse command line arguments
+	int N = stoi(argv[1]);
+
+	// Create order list from file
+	OrderList orders;
+	orders.add_from_file("sales.txt", N);
+
+	// Start time
+	auto start = chrono::high_resolution_clock::now();
+
+	// Sort sales
+	orders.sort();
+
+	// End time
+	auto stop = chrono::high_resolution_clock::now();
+
+	// Calculate duration
+	auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
+
+	// Print elapsed time in microseconds
+	cout << N << " sales are sorted in " << duration.count() << " microseconds." << endl;
+
 	return 0;
 }
